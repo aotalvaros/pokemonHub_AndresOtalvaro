0ce5eb74d1ff4170ac014a50d5484122d32cc5ca
Andres Otalvaro <andr3s.o7alvaro@gmail.com>
2025-10-22 21:45:20 -0500

    Configuracion para pruebas unitarias

---
+++ src/test/setup.ts
@@ -0,0 +1,18 @@
+import { afterEach, beforeEach, vi } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+
+beforeEach(() => {
+  vi.mock("react-redux", async () => {
+    const actual = await vi.importActual("react-redux");
+    return {
+      ...actual,
+      useDispatch: () => vi.fn(),
+      useSelector: vi.fn(),
+    };
+  });
+});
+
+afterEach(() => {
+  cleanup();
+});
